f0735d6ed6a659c41520de1211bcbc19db16166a
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2023-04-14 18:45:40 +0100

    Exercise 2-2: directives and statements

---
+++ chapter2/exercise2-2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>  // directive
+
+/* Conside this program.
+ *  a) identify the directives and statements in this program.
+ *  b) what ouput does the program produce?
+ */
+
+/* The program produces the following output:
+ * Parkinson's Law:
+ * Work expands so as to fill the time
+ * available for its completion.
+ */
+
+int main()
+{
+    printf("Parkinson's Law:\nWork expands so as to ");  // statement
+    printf("fill the time\n");  // statement
+    printf("available for its completion.\n");  // statement
+    return 0;  // statement
+}
