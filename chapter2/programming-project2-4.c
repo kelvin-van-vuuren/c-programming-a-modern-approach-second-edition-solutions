c601bc288625620138ce8e5f5efd940437af3c5a
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2023-04-15 12:13:12 +0100

    pp2-4: add tax to given amount
    Read amount via scanf then use printf to display amount with 5% tax
    added.

---
+++ chapter2/programming-project2-4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* Write a program that asks the user to enter a dollars-and-cents amount, then displays the amount with 5% tax added.
+ */
+
+int main()
+{
+    float amount, tax;
+    
+    amount = 0;
+    printf("Enter an amount: ");
+    scanf("%f", &amount);
+
+    tax = amount * 1.05;
+    printf("With tax added: %f\n", tax);
+}
