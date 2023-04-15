bd03c18c9a8bcdf89f8148c31b1c4956230654cb
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2023-04-15 11:28:10 +0100

    Exercise 2-8: tokens
    The statement 'answer=(3*q-p*p);' is consists of 12 tokens.

---
+++ chapter2/exercise2-8.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* How many tokens are there in the following statement?:
+ * answer=(3*q-p*p)/3;
+ */
+
+int main()
+{
+    printf(
+        "C programs can be thought of as a series of tokens: groups of characters that can't be split up without "
+        "changing their meaning.\n");
+    printf("The statement 'answer=(3*q-p*p);' is consists of 12 tokens.\n");
+}
