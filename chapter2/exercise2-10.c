0aec50fcecdab5a978cec5f965824897a892a2bd
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2023-04-15 11:39:12 +0100

    Exercise 2-10: dweight.c essential spaces
    In dweight.c, the essential spaces are following keywords and within
    lines containing directives.

---
+++ chapter2/exercise2-10.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+/* In the dweight.c program, which spaces are essential? */
+
+int main()
+{
+    printf("In dweight.c the essential spaces are following keywords and within lines containing directives.\n");
+}
