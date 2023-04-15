30b247eac1e19fa3a0d2d20cb6da8abcecbbb632
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2023-04-15 10:56:44 +0100

    Exercise 2-6: adjacent underscores
    It can be difficult to determine how many adjacent underscores are
    present when there is more than one. This reduces the program's
    readability and therefore is not a good practice.

---
+++ chapter2/exercise2-6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* Why is it not a good idea for an identifier to contain more than one adjacent underscore (as in current___balance,
+ * for example)? */
+
+int main()
+{
+    printf(
+        "It can be difficult to determine how many adjacent underscores are present when there is more than one. This "
+        "reduces the program's readability and therefore is not good practice.\n");
+}
