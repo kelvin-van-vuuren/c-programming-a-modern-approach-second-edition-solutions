46f0119e6d1a32a9dda270e43f5c6329669b43fb
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2023-04-15 11:43:59 +0100

    pp2-1: printf
    Program that uses printf to display the following picture on the screen:
                  *
                *
              *
    *       *
      *   *
        *

---
+++ chapter2/programming-project2-1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* Write a program that uses printf to display the following picture on the screen:
+* 
+*               *
+*             *
+*           *
+* *       *
+*   *   *
+*     *
+*/
+
+int main()
+{
+    printf("               *\n");
+    printf("             *\n");
+    printf("           *\n");
+    printf(" *       *\n");
+    printf("   *   *\n");
+    printf("     *\n");
+}
