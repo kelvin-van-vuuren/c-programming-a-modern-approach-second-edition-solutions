79c7f879ddb1ba707e3190a186787cf8d17c83ba
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2023-04-15 11:33:42 +0100

    Exercise 2-9: spaces between tokens

---
+++ chapter2/exercise2-9.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+/* Insert spaces between the tokens in exercise 2-8 to make the statement easier to read. */
+
+int main()
+{
+    printf("Statement with spaces between token: (3 * q - p * p) / 3;\n");
+}
