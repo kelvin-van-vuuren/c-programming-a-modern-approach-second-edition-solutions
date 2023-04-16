0dda347db6c7435d363e63aaff1c84b6c69e7090
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2023-04-16 09:30:34 +0100

    pp2-5: calc polynomial
    Ask user for value of x using scanf then calculate:
    3x^5 + 2x^4 - 5x^3 - x^2 + 7x - 6

---
+++ chapter2/programming-project2-5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* Write a program that asks the user to enter a value for x and then displays the value of the following polynomial:
+ * 3x^5 + 2x^4 - 5x^3 - x^2 + 7x - 6
+ */
+
+int main()
+{
+    float x = 0;
+    printf("Enter value for x: ");
+    scanf("%f", &x);
+    
+    float answer = 3 * (x * x * x * x * x) + 2 * (x * x * x * x) - 5 * (x * x * x) - (x * x) + 7 * x - 6;
+    printf("3x^5 + 2x^4 - 5x^3 - x^2 + 7x - 6 = %f\n", answer);
+}
