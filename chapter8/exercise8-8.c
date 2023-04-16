2a7e4732149e5ad07cd3ff91bd9f66b33f574746
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2023-04-16 18:05:50 +0100

    Exercise 8-8: 2d array 30 x 24
    For storing a month's (30 days) worth of hourly temperature readings.

---
+++ chapter8/exercise8-8.c
@@ -0,0 +1,9 @@
+/* Write a declaration for a two-dimensional array named temperature_readings that stores one month of hourly
+ * temperature readings. (For simplicity, assume that a month has 30 days.) The rows of the array should represent days
+ * of the month; the columns should represent hours of the day.
+ */
+
+#define DAYS 30
+#define HOURS 24
+
+int main() { int temperature_readings[DAYS][HOURS]; }
