7aaadc14baf929b0f1dcbc602f6afa8d4777bed8
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2023-04-22 16:42:03 +0100

    pp12-3: pp12-1 array name as ptr

---
+++ chapter12/programming-project12-3.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+/* Simplify programming project 1 by taking advantage of the fact that an array name can be used as a pointer
+ */
+
+int main() { printf("Solution to programming project 12-1 already makes use of array name as a pointer.\n"); }
