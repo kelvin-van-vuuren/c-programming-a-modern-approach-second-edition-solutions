ebdc756f52c987126bdf958a34a5894d7cb38a74
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2023-05-04 16:12:12 +0100

    Exercise 3-2: printf

---
+++ chapter3/exercise3-2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* Write calls of printf that display a float variable x in the following formats:
+ * a) Exponential notation; left-justified in a field of size 8; one digit after the decimal point.
+ * b) Exponential notation; right-justified in a field of size 10; six digits after the decimal point.
+ * c) Fixed decimal notation; left-justified in a field of size 8; three digits after the decimal point.
+ * d) Fixed decimal notation; right-justified in a field of size 6; no digits after the decimal point.
+ */
+
+int main()
+{
+    float x;
+
+    x = 1.01;
+
+    printf("a) %-8.1e\n", x);
+    printf("b) %10.6e\n", x);
+    printf("c) %-8.3f\n", x);
+    printf("d) %6.0f\n", x);
+}
