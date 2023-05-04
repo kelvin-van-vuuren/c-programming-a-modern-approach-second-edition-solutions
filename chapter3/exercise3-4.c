2c07e36bae02fa1b35ed55832d33749a2ec92abf
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2023-05-04 16:52:47 +0100

    Exercise 3-4: scanf int vs float

---
+++ chapter3/exercise3-4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* Suppose that we call scanf as follows:
+ * scanf("%d%f%d" &i, &x, &j);
+ * If the user enter:
+ * 10.3 5 6
+ * what will the values of i, x and j be after the call? (assume i and j are int variables and x is a float variable).
+ */
+
+int main() { printf("i = 10, x = .3, j = 5\n"); }
