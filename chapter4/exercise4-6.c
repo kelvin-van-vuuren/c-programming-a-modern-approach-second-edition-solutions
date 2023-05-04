4fb527fb47dd5764f57f659872195115340ccc49
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2023-05-04 21:40:48 +0100

    Exercise 4-6: C99 negative modulo

---
+++ chapter4/exercise4-6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* What is the value of each of the following expressions in C99?
+ * a) 8 % 5
+ * b) -8 % 5
+ * c) 8 % -5
+ * d -8 % -5
+ */
+
+int main()
+{
+    printf("a) 3\n");
+    printf("b) -3\n");
+    printf("c) 3\n");
+    printf("d) 3\n");
+}
