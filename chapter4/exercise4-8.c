185fccf4436a1ac783602d6c6fa6481e3fcc4bd0
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2023-05-04 21:45:56 +0100

    Exercise 4-8: upc.c

---
+++ chapter4/exercise4-8.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+/* Would the upc.c program still work if the expression 9 - ((total - 1) % 10) were replaced by (10 - (total % 10)) % 10
+ */
+
+int main() { printf("Both expressions are equivalent\n"); }
