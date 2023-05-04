d3953ad37ffb15ade8b456a82fb0cc82314a3bc5
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2023-05-04 17:00:43 +0100

    Exercise 3-6: addfrac.c whitespace matching

---
+++ chapter3/exercise3-6.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* Show how to modify the addfrac.c program of Section 3.2 so that the user is allowed to enter fractions that contain
+ * spaces before and after each / character.
+ */
+
+int main()
+{
+    printf("Adding a space after the first %%d of each scanf will allow pattern matching between integers and /.\n");
+}
