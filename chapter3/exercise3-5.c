172aad5b48754036376d0b4c3043dcf734ba9751
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2023-05-04 16:55:02 +0100

    Exercise 3-5: scanf float vs int

---
+++ chapter3/exercise3-5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* Suppose that we call scanf as follows:
+ * scanf("%f%d%f", &x, &i, &y);
+ * If the user enters:
+ * 12.3 45.6 789
+ * What will be the values of x, i, and y after the call? (Assume that x and y are float variables and i is an int
+ * variable)
+ */
+
+int main() { printf("x = 12.3, i = 45, y = 0.6"); }
