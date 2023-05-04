27022557fc34142466c31f93c0c298821b705ef7
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2023-05-04 21:32:30 +0100

    Exercise 4-3: C89 negative number division

---
+++ chapter4/exercise4-3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* What is the value of each of the following expresssion in C98?
+ * a) 8 / 5
+ * b) -8 / 5
+ * c) 8 / -5
+ * d) -8 / -5
+ * */
+
+int main()
+{
+    printf("a) 1\n");
+    printf("b) -1 or -2\n");
+    printf("c) -1 or -2\n");
+    printf("d) 1 or 2\n");
+}
