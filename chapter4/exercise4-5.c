e1c21863b9e7befe94891d1ebd932f88ac7a63ee
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2023-05-04 21:39:49 +0100

    Exercise 4-5: C89 negative modulo

---
+++ chapter4/exercise4-5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* What is the value of each of the following expressions in C89?
+ * a) 8 % 5
+ * b) -8 % 5
+ * c) 8 % -5
+ * d -8 % -5
+ */
+
+int main()
+{
+    printf("a) 3\n");
+    printf("b) -3 or 5\n");
+    printf("c) 3 or -5\n");
+    printf("d) 3 or 5\n");
+}
