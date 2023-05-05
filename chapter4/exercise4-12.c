02a34e60bc4e96771c0151937e6466b42adbe23e
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2023-05-05 13:21:33 +0100

    Exercise 4-12: more incr / decr

---
+++ chapter4/exercise4-12.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* Show the output produced by each of the following program fragments. Assume that i and j are int variables. */
+
+int main()
+{
+    printf("a) 6 16\n");
+    printf("b) 6 -7\n");
+    printf("c) 6 23\n");
+    printf("d) 6 15\n");
+}
