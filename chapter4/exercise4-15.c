143a4f31af2bc42cc01b92b1e84ee76e0ecf38e4
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2023-05-05 13:31:40 +0100

    Exercise 4-15: int values after expressions

---
+++ chapter4/exercise4-15.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* Give the values of i and j after each of the following expression statements has been executed. Assume that i has the
+ * value 1 initially and j has the value 2. */
+
+int main()
+{
+    printf("a) i = 3, j = 2\n");
+    printf("b) i = 2, j = 2\n");
+    printf("c) i = 2, j = 2\n");
+    printf("d) i = 2, j = 3\n");
+}
