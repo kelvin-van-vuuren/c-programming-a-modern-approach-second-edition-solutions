17f0f3ce1b9e6ec4fb85e0fe4dbff2ba46c8d428
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2023-05-05 13:28:22 +0100

    Exercise 4-14: operator precedence

---
+++ chapter4/exercise4-14.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* Supply parentheses to show how a C compiler would interpret each of the following expressions. */
+
+int main()
+{
+    printf("a) ((a * b) - (c * d)) + e\n");
+    printf("b) ((a / b) %% c) / d\n");
+    printf("c) (((- a) - b) + c) - (+ d)\n");
+    printf("d) ((a * (-b) / c) - d\n");
+}
