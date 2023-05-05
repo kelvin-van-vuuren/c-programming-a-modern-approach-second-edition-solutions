6683d89f0762a1d08d90a7097af989f0a7f67829
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2023-05-05 13:07:18 +0100

    Exercise 4-9: compound assignments

---
+++ chapter4/exercise4-9.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* Show the output produced by each of the following program fragments. Assume that i, j, and k are int variables. */
+
+int main()
+{
+    printf("a) 63\n");
+    printf("b) 3 2 1\n");
+    printf("c) 2 -1 3\n");
+    printf("d) 0 0 0\n");
+}
