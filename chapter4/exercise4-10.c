9c086ccdea827b4441f43983ce59aebd5fa41d82
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2023-05-05 13:11:17 +0100

    Exercise 4-10: assignment subexpressions

---
+++ chapter4/exercise4-10.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* Show the output produced by each of the following program fragments. Assume that i and j are int variables. */
+
+int main()
+{
+    printf("a) 7 7\n");
+    printf("b) 3 4\n");
+    printf("c) 2 8\n");
+    printf("d) 6 9\n");
+}
