609605db3ed49f3122260628858661cc4b656bb0
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2023-05-05 13:15:55 +0100

    Exercise 4-11: incr / decr within expressions

---
+++ chapter4/exercise4-11.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* Show the output produced by each of the following program fragments. Assume that i, j, and k are int variables. */
+
+int main()
+{
+    printf("a) 0 2\n");
+    printf("b) 4 11 6\n");
+    printf("c) 0 8 7\n");
+    printf("d) 3 4 5 4\n");
+}
